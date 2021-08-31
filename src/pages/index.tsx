88a51aaeaa007d069aba6b6bcf310b2f899c14e0
José Oliveira <j.oliveira90@live.com>
2021-08-31 12:50:18 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>HELLO</h1>
+    </div>
+  );
+}
